22ad532afe8b2ee84913658042cc7481862b60c4
kei kumabayashi <kkumabayashi.electro@gmail.com>
2021-12-11 10:30:50 +0000

    １回目

---
+++ human.rb
@@ -0,0 +1,15 @@
+require './animal'
+require './thinkable'
+
+class Human < Animal
+    attr_accessor :name, :age, :hobby,
+    
+    def initialize(name, age, hobby)
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+    end
+    
+    include Thinkable
+    
+end
